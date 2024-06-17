b27cf349f2cb231ed9c09c5e7adf312a1cfbe4a1
Alexander Kraehan <wi22b070@technikum-wien.at>
2024-06-18 01:39:41 +0200

    fixed login, added profile features for changing password, refactored invoice creation, enabled auto-login on registration

--- Frontend/js/invoice.js
+++ Frontend/js/invoice.js
@@ -11,11 +11,12 @@ $(document).ready(function() {
             url: apiUrl,
             success: function (response) {
 
-                if (response && response.order_date) {
+                if (response[0] && response[0].orderdate) {
 
                     //console.log(response);
                     displayInvoice(response);
                 } else {
+
                     console.log("Keine Produkte gefunden");
                 }
             },
@@ -27,31 +28,40 @@ $(document).ready(function() {
 
 
 
-function displayInvoice(order){
-    var invoice = `        <h1>Rechnung</h1>
-            <p>Rechnungsnummer: INV-${order.productid}</p>
-            <p>Datum: ${order.order_date}</p>
-            <p>Anschrift:</p>
-            <p>${order.salutation} ${order.firstname} ${order.lastname}</p>
-            <p>${order.address}</p>
-            <p>${order.plz} ${order.city}</p>
+    function displayInvoice(orders) {
+        // Initialize an empty string to store the invoice HTML
+        let invoice = `<h1>Rechnung</h1>
+<p><u>Rechnungsnummer: INV-${orders[0].order_id}</u></p>
+            <p><u>Datum: ${orders[0].orderdate}</u></p>
+            <p><u>Anschrift:</u></p>
+            <p>${orders[0].salutation} ${orders[0].firstname} ${orders[0].lastname}</p>
+            <p>${orders[0].address}</p>
+            <p>${orders[0].plz} ${orders[0].city}</p>
 
             <h2>Bestellpositionen</h2>
-            <table border="1">
+<table border="1">
                 <tr>
                     <th>Artikel</th>
                     <th>Stückzahl</th>
                     <th>Preis</th>
                 </tr>
-                <tr>
-                    <td>${order.productname}</td>
-                    <td>${order.productCount}</td>
-                    <td>${order.totalPrice.toFixed(2)} €</td>
-                </tr>
-            </table>
+`;
 
-            <h3>Status: ${order.state}</h3>`;
-    console.log(invoice)
-    $('.container.mt-5').append(invoice);
-}
+        // Loop through each order
+        orders.forEach((order, index) => {
+            invoice +=
+                `<tr><td>${order.productname}</td>
+                   <td style="text-align: center;">${order.productCount}</td>
+                 <td>${order.totalPrice.toFixed(2)} €</td></tr>
+        `
+        });
+        invoice+=`
+<td style="border-top: 2px solid black;"><b><u>Total:</b></u></td>
+<td style="border-top: 2px solid black; text-align: center;">EUR</td>
+<td style="border-top: 2px solid black;">${orders[0].total}</td>
+</table>`;
+        // Append the invoice to the container (assuming you have an element with class 'container.mt-5')
+        $('.container.mt-5').append(invoice);
+    }
+
 })
